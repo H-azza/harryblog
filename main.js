b368f8346c3c0a4ac0654acc8e4991e2049ba18f
Harrison Taylor <HarrisonTaylor@Harrisons-MacBook-Pro-262.local>
2019-10-25 14:12:30 +1300

    Website blog to be hosted

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-// let test = true
-//
-// console.log(test)
-let randomNumber = Math.floor(Math.random()*100+1)
-console.log(randomNumber)
 
-var numGuesses = 0
+let randomNumber = Math.floor(Math.random()*100)
+console.log(randomNumber)
+let numGuesses = 0;
+
 
 
 function compare() {
   let guess = document.getElementById("number").value
+  let gap = Math.abs(guess - randomNumber)
 
-  if (guess > randomNumber) {
-    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is greater than our number.`
+
+  if (gap > 50) {
+    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is feeling freezing cold.`
     numGuesses ++
     document.getElementById('tries').innerHTML = `<h2>Number of tries: ${numGuesses}</h2>`
-  }else if (guess < randomNumber) {
-    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is less than our number.`
+  }else if (gap > 20) {
+    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is feeling cold.`
     numGuesses ++
     document.getElementById('tries').innerHTML = `<h2>Number of tries: ${numGuesses}</h2>`
-  } else {
+  }else if (gap > 10) {
+    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is feeling warm.`
+    numGuesses ++
+    document.getElementById('tries').innerHTML = `<h2>Number of tries: ${numGuesses}</h2>`
+  }else if (gap > 5) {
+    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is feeling hot.`
+    numGuesses ++
+    document.getElementById('tries').innerHTML = `<h2>Number of tries: ${numGuesses}</h2>`
+  }else if (gap >= 1) {
+    document.getElementById('closeness').innerHTML = `Your number: ${document.getElementById("number").value} is feeling BOILING.`
+    numGuesses ++
+    document.getElementById('tries').innerHTML = `<h2>Number of tries: ${numGuesses}</h2>`
+  }else {
     document.getElementById('closeness').innerHTML = `You got it!`
+  }
+
 }
 
+function resetGame(){
+  randomNumber = Math.floor(Math.random()*100)
+  document.getElementById('closeness').innerHTML = `Good Luck`
+
+  numGuesses = 0
+  document.getElementById('tries').innerHTML = `<h2>Number of tries: ${numGuesses}</h2>`
+
+  console.log(randomNumber)
 }
